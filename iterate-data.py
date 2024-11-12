ab44efafa873bbf3773e0848f35acc620c5059e1
harshgarg22 <gargh2202@gmail.com>
2024-11-13 00:47:05 +0400

    Extracted data from json

--- iterate-data.py
+++ iterate-data.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-
+import json
 class IterateData:
     
     def CreateDataset(self, path):
@@ -9,21 +9,30 @@ class IterateData:
             "Age": [],
             "Gender":[],
             "Location": [],
-            "Recording Number": []
+            "Recording Number": [],
+            "Start (in ms)":[],
+            "End (in ms)": [],
+            "Type of Event":[]
         }
         
         for filename in os.listdir(path):
             if filename.__contains__(".json"):
-                filename = filename.replace(".json", "")
-                patient_id, age, gender, location, recording_number = filename.split("_")
-                data["Patient ID"].append(patient_id)
-                data["Age"].append(age)
-                data["Gender"].append(gender)
-                data["Location"].append(location)
-                data["Recording Number"].append(recording_number)
-        
+                json_path = path + "/" + filename
+                with open(json_path) as f:
+                    info = json.load(f)
+                    filename = filename.replace(".json", "")
+                    patient_id, age, gender, location, recording_number = filename.split("_")
+                    for i in info["event_annotation"]:
+                        data["Patient ID"].append(patient_id)
+                        data["Age"].append(age)
+                        data["Gender"].append(gender)
+                        data["Location"].append(location)
+                        data["Recording Number"].append(recording_number)
+                        data["Start (in ms)"].append(i["start"])
+                        data["End (in ms)"].append(i["end"])
+                        data["Type of Event"].append(i["type"])
+                                       
         df = pd.DataFrame(data)
-        
         return df
     
 
